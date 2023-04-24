dee06a3060687bea054cece3991fad6a96511349
KatushaKononova56 <83833827+KatushaKononova56@users.noreply.github.com>
2023-04-24 12:25:29 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 var date = new Date(); // создаем объект Date с текущей датой и временем
 var h = date.getHours(); // получаем текущий час
 
-if (h > 11 || h < 8) document.getElementById("stylesheet").href = "./css/night.css"; 
+if (h > 20 || h < 8) document.getElementById("stylesheet").href = "./css/night.css"; 
 // если сейчас ночь, то меняем файл стилейFF
